4d5757d4d4fbe7fb7fb9a4e6e425a6a9e32fd6d6
Damien Detcherry <damien@phonoblaster.com>
2011-10-07 10:55:24 +0200

    Pickle removed from home page (retrieving keys for contributions in bulk)

--- controllers/home.py
+++ controllers/home.py
@@ -10,21 +10,23 @@ class HomeHandler(BaseHandler):
 		current_user_station = None
 		if(self.current_user):
 			current_user_station = Station.all().filter("creator", self.current_user.key()).get()
-			
+						
 		active_stations = Station.all().filter("active >", datetime.now()).order("-active").fetch(48)
 		non_active_stations = None
 		if(len(active_stations) == 0):
 			non_active_stations = Station.all().order("-active").fetch(48)
 		
-		current_user_contributions = None
+		current_user_contributing_stations = None
 		if(self.current_user):
 			current_user_contributions = Contribution.all().filter("contributor", self.current_user.key()).fetch(12)
+			station_keys = [Contribution.station.get_value_for_datastore(c) for c in current_user_contributions]
+			current_user_contributing_stations = db.get(station_keys)
 		
 		self.additional_template_values = {
 			"current_user_station": current_user_station,
 			"active_stations": active_stations,
 			"non_active_stations": non_active_stations,
-			"current_user_contributions": current_user_contributions,
+			"current_user_contributing_stations": current_user_contributing_stations,
 		}
 		self.render("../templates/home.html")
 		
