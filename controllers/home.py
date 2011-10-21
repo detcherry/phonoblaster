add9b0f61dadb1ba4428fc7ca20647c3ee47fd96
Damien Detcherry <damien@phonoblaster.com>
2011-10-21 10:39:45 +0200

    Beginning of the new welcome page for unlogged users

--- controllers/home.py
+++ controllers/home.py
@@ -8,19 +8,30 @@ from models.db.contribution import Contribution
 class HomeHandler(BaseHandler):
     def get(self):
 		current_user_station = None
+		# We get the user station
 		if(self.current_user):
 			current_user_station = Station.all().filter("creator", self.current_user.key()).get()
-						
-		active_stations = Station.all().filter("active >", datetime.now()).order("-active").fetch(48)
-		non_active_stations = None
-		if(len(active_stations) == 0):
-			non_active_stations = Station.all().order("-active").fetch(48)
 		
+		# We get the stations where the user contributes
 		current_user_contributing_stations = None
 		if(self.current_user):
 			current_user_contributions = Contribution.all().filter("contributor", self.current_user.key()).fetch(12)
 			station_keys = [Contribution.station.get_value_for_datastore(c) for c in current_user_contributions]
 			current_user_contributing_stations = db.get(station_keys)
+		
+		
+		# If user is logged in, we fetched up to 48 stations. Otherwise just 12
+		if(self.current_user):
+			number_of_stations_to_fetch = 48
+		else:
+			number_of_stations_to_fetch = 12
+			
+		# We get the active or non active stations	
+		active_stations = Station.all().filter("active >", datetime.now()).order("-active").fetch(number_of_stations_to_fetch)
+		non_active_stations = None
+		if(len(active_stations) == 0):
+			non_active_stations = Station.all().order("-active").fetch(number_of_stations_to_fetch)
+		
 		
 		self.additional_template_values = {
 			"current_user_station": current_user_station,
