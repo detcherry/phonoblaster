eeef9582774a8811b26bba2d3b56df1b28c341ca
Damien Detcherry <damien@phonoblaster.com>
2011-11-02 17:33:02 +0100

    Now: - when users are not logged in the tab "add tracks" is displayed nevertheless - when users are logged in but not a contributor, they are invited to either 	* post to their station 	* create their station

--- controllers/station/station.py
+++ controllers/station/station.py
@@ -14,32 +14,28 @@ class StationHandler(RootStationHandler):
 		if not self.current_station:
 			self.redirect("/error/404")
 		else:
-			# When you're the station creator or a contributor, you'll always have access to!
-			if(self.allowed_to_post):
-				logging.info("Allowed to post, doesn't matter if station too crowded")
+			# Retrieve the number of listeners
+			number_of_listeners = len(self.station_proxy.station_sessions)
+			
+			if(self.allowed_to_post or number_of_listeners < 100):
+				logging.info("Allowed to post or station not too crowded")
+				
+				# Probably not efficient, will require a refactoring at some point using the memcache
+				self.user_station = None
+				if(self.current_user):
+					self.user_station = Station.all().filter("creator", self.current_user.key()).get()
+				
 				self.additional_template_values = {
 					"site_url": controllers.config.SITE_URL,
 					"allowed_to_post": self.allowed_to_post,
 					"status_creator": self.status_creator,
+					"user_station": self.user_station,
 				}		
 				self.render("../../templates/station/station.html")
-				
 			else:
-				# Retrieve the number of listeners
-				number_of_listeners = len(self.station_proxy.station_sessions)
-			
-				if(number_of_listeners < 100):
-					logging.info("Number of listeners < 100")
-					self.additional_template_values = {
-						"site_url": controllers.config.SITE_URL,
-						"allowed_to_post": self.allowed_to_post,
-						"status_creator": self.status_creator,
-					}		
-					self.render("../../templates/station/station.html")
-				else:
-					logging.info("Number of listeners > 100, station too crowded")
-					self.additional_template_values = {}
-					self.render("../../templates/station/toocrowded.html")
+				logging.info("Number of listeners > 100, station too crowded")
+				self.additional_template_values = {}
+				self.render("../../templates/station/toocrowded.html")
 				
 
 application = webapp.WSGIApplication([
--- static/js/station/tab.js
+++ static/js/station/tab.js
@@ -11,34 +11,39 @@ tabController.prototype = {
 	init: function(){
 		
 		// Reveal initial content area(s).
-	 	$("#tab_content_wrap")
-			.each(function(){
-				$(this).find(".tab_content:first").show();
-			});
+	 	//$("#tab_content_wrap")
+		//	.each(function(){
+		//		$(this).find(".tab_content:first").show();
+		//	});
 		
-			// Listen for click on tabs.
-		 	$("#tabs a").click(function() {
-				// If not current tab. 
-				if (!$(this).hasClass("current")) {
-					// Change the current indicator.
-					 $(this)
-						.addClass("current")
-						.parent("li")
-						.siblings("li")
-						.find("a.current")
-						.removeClass("current");
-					
-					// Show target, hide others.
-					 $($(this).attr('href'))
-						.show()
-						.siblings(".tab_content")
-						.hide();
-				}
+		//Find the tab that is current
+		// Unlike the previous code, this code is valid only if there is one tab system per page
+		current_tab = $("#tabs a.current").attr("href");
+		$(current_tab).show();
+		
+		// Listen for click on tabs.
+	 	$("#tabs a").click(function() {
+			// If not current tab. 
+			if (!$(this).hasClass("current")) {
+				// Change the current indicator.
+				 $(this)
+					.addClass("current")
+					.parent("li")
+					.siblings("li")
+					.find("a.current")
+					.removeClass("current");
 				
-				// Nofollow.
-				 this.blur();
-				 return false;
-			});
+				// Show target, hide others.
+				 $($(this).attr('href'))
+					.show()
+					.siblings(".tab_content")
+					.hide();
+			}
+			
+			// Nofollow.
+			 this.blur();
+			 return false;
+		});
 		
 	},
 	
