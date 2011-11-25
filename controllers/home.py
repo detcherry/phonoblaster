e3b22495bedad28fbee965197a90862a6d878447
Damien Detcherry <damien@phonoblaster.com>
2011-11-25 13:44:28 +0100

    First design welcome page

--- controllers/home.py
+++ controllers/home.py
@@ -5,4 +5,4 @@ class HomeHandler(BaseHandler):
 		template_values = {}
 		if(self.current_user):
 			template_values = { "id": self.current_user.twitter_id }
-		self.render("base.html", template_values)
+		self.render("home.html", template_values)
