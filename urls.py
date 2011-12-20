3bff309d573a2559a9fe55030d9ecd5735eb775b
Damien Detcherry <damien@phonoblaster.com>
2011-12-20 17:13:21 +0100

    A new way to handle templates

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from google.appengine.dist import use_library
 use_library('django', '1.2')
+from django.conf import settings
+settings.configure(INSTALLED_APPS=('settings',))
 
-import logging	
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
@@ -11,12 +11,14 @@ from controllers.home import HomeHandler
 from controllers.all import AllHandler
 from controllers.station.create import StationCreateHandler
 from controllers.station.check import StationCheckHandler
+from controllers.station.station import StationHandler
 
 application = webapp.WSGIApplication(
 	[
 		('/', HomeHandler),
 		('/station/create', StationCreateHandler),
 		('/station/check', StationCheckHandler),
+		('/(\w+)', StationHandler),
 		('/.*', AllHandler),
 	],
     debug=True)
