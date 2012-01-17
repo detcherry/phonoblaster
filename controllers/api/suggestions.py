b3bd2196e22e0f0b2b5aae86dbc76cf3b6d06743
Damien Detcherry <damien@phonoblaster.com>
2012-01-17 19:38:15 +0100

    Now we check if the user didn't suggest something shortly

--- controllers/api/suggestions.py
+++ controllers/api/suggestions.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+from datetime import timedelta
 from django.utils import simplejson as json
 
 from google.appengine.api.taskqueue import Task
@@ -40,28 +42,39 @@ class ApiSuggestionsHandler(BaseHandler):
 			track = None
 			extended_track = None
 			
-			if(suggestion["track_id"]):
-				track = Track.get_by_id(int(suggestion["track_id"]))
-				# If track on Phonoblaster, get extended track from Youtube
-				if(track):
-					extended_track = Track.get_extended_tracks([track])[0]
-			else:
-				# If obviously not, look for it though, save it otherwise and get extended track from Youtube
-				if(suggestion["youtube_id"]):
-					track, extended_track = Track.get_or_insert_by_youtube_id(suggestion["youtube_id"], station, user, admin)
+			# Check when the user submitted his last suggestion
+			q = Suggestion.all()
+			q.filter("user", user)
+			q.filter("station", station)
+			q.filter("created >", datetime.utcnow() - timedelta(0,180))
+			user_last_suggestion = q.get()
 			
-			if(track and extended_track):
-				suggestion = Suggestion(
-					key_name = suggestion["key_name"],
-					track = track.key(),
-					station = station.key(),
-					message = suggestion["message"][:140],
-				)
-				
-				suggestion.put()
-				logging.info("New suggestion saved into the datastore")
-				
-				extended_suggestion = Suggestion.get_extended_suggestion(suggestion, extended_track, user)
+			if(user_last_suggestion):
+				logging.info("User submitted a suggestion shortly")
+			else:
+				logging.info("User did not submit a suggestion shortly")
+				if(suggestion["track_id"]):
+					track = Track.get_by_id(int(suggestion["track_id"]))
+					# If track on Phonoblaster, get extended track from Youtube
+					if(track):
+						extended_track = Track.get_extended_tracks([track])[0]
+				else:
+					# If obviously not, look for it though, save it otherwise and get extended track from Youtube
+					if(suggestion["youtube_id"]):
+						track, extended_track = Track.get_or_insert_by_youtube_id(suggestion["youtube_id"], station, user, admin)
+		
+				if(track and extended_track):
+					suggestion = Suggestion(
+						key_name = suggestion["key_name"],
+						track = track.key(),
+						station = station.key(),
+						message = suggestion["message"][:140],
+					)
+			
+					suggestion.put()
+					logging.info("New suggestion saved into the datastore")
+			
+					extended_suggestion = Suggestion.get_extended_suggestion(suggestion, extended_track, user)
 		
 		response = False
 		if(extended_suggestion):
