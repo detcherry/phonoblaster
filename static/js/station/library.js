ddec22ca800d3cb82271363ea81788f0510ca8f1
Damien Detcherry <damien@phonoblaster.com>
2012-01-19 10:43:09 +0100

    Forgot to write the "track submitter" of tracks in the library (either station if user is admin either user)

--- static/js/station/library.js
+++ static/js/station/library.js
@@ -25,6 +25,21 @@ function LibraryManager(station_client){
 }
 
 LibraryManager.prototype.serverToLocalItem = function(content){
+	content["type"] = "track";
+	
+	if(this.station_client.admin){
+		content["track_admin"] = true;
+		content["track_submitter_key_name"] = this.station_client.station.key_name;
+		content["track_submitter_name"] = this.station_client.station.name;
+		content["track_submitter_url"] = "/" + this.station_client.station.shortname;
+	}
+	else{
+		content["track_admin"] = false;
+		content["track_submitter_key_name"] = this.station_client.user.key_name;
+		content["track_submitter_name"] = this.station_client.user.name;
+		content["track_submitter_url"] = "/user/" + this.station_client.user.key_name;
+	}
+	
 	var item = {
 		id: content.track_id,
 		created: content.track_created,
