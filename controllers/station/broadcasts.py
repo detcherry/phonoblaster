3ff8e6dff7d0422288f2504cc13fb53ee37cf2b2
Damien Detcherry <damien@phonoblaster.com>
2012-02-01 18:12:06 +0100

    REdirect users from broadcasts to station when on the air!

--- controllers/station/broadcasts.py
+++ controllers/station/broadcasts.py
@@ -5,4 +5,14 @@ from models.api.station import StationApi
 class StationBroadcastsHandler(RootHandler):
 	def get(self, shortname):
 		self.station_proxy = StationApi(shortname)
-		self.render("station/off.html", None)
+		
+		if(self.station_proxy.station):
+			on_air = self.station_proxy.on_air()
+			
+			if(on_air):
+				self.redirect("/"+shortname)
+			else:
+				self.render("station/off.html", None)
+			
+		else:
+			self.render("station/404.html", None)
