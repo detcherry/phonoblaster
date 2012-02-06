98b189257d847cc4a28f590f548835bf810139dc
Damien Detcherry <damien@phonoblaster.com>
2012-02-06 22:25:24 +0100

    Finished implementing off page for Facebook tabs

--- static/js/station/off/off.js
+++ static/js/station/off/off.js
@@ -1,20 +1,25 @@
 $(function(){
-	STATION_CLIENT = new StationClient(USER, ADMIN, STATION)
+	STATION_CLIENT = new StationClient(USER, ADMIN, STATION, WEBSITE)
 })
 
-function StationClient(user, admin, station){
+function StationClient(user, admin, station, website){
 	this.user = user;
 	this.admin = admin;
 	this.station = station;
-	
-	this.broadcasts_counter = new Counter("#broadcasts");
-	this.views_counter = new Counter("#views");
+	this.website = website;
 	
 	this.broadcast_manager = new BroadcastManager(this);
-	this.comment_manager = new CommentManager(this);
-	this.air_manager = new AirManager(this);
 	
-	this.comment_manager.get(); // Force comments to be fetched
-	
+	// If on Phonoblaster website, these elements need to be loaded
+	if(this.website){
+		this.air_manager = new AirManager(this);
+
+		this.comment_manager = new CommentManager(this);
+		this.comment_manager.get(); // Force comments to be fetched
+
+		this.broadcasts_counter = new Counter("#broadcasts");
+		this.views_counter = new Counter("#views");
+	}
+
 	this.favorite_sdk = new FavoriteSDK(this.broadcast_manager);
 }
