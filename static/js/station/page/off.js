96b44a05c9a4e906ff46c0eaf06165b897e131f2
Damien Detcherry <damien@phonoblaster.com>
2012-02-06 22:32:22 +0100

    Better files separation between page and website off modes

--- static/js/station/page/off.js
+++ static/js/station/page/off.js
@@ -7,14 +7,6 @@ function StationClient(user, admin, station){
 	this.admin = admin;
 	this.station = station;
 	
-	this.broadcasts_counter = new Counter("#broadcasts");
-	this.views_counter = new Counter("#views");
-	
 	this.broadcast_manager = new BroadcastManager(this);
-	this.air_manager = new AirManager(this);
-
-	this.comment_manager = new CommentManager(this);
-	this.comment_manager.get(); // Force comments to be fetched
-
 	this.favorite_sdk = new FavoriteSDK(this.broadcast_manager);
 }
