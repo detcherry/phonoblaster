ecf04349bcb6b97a17912c2bc6981518d5aef8cc
Damien Detcherry <damien@phonoblaster.com>
2012-02-08 16:04:07 +0100

    Better inheritance in iFrame app for OFF MODE

--- static/js/station/off/off.js
+++ static/js/station/off/off.js
@@ -3,18 +3,26 @@ $(function(){
 })
 
 function StationClient(user, admin, station){
-	this.user = user;
-	this.admin = admin;
-	this.station = station;
-	
-	this.broadcasts_counter = new Counter("#broadcasts");
-	this.views_counter = new Counter("#views");
-	
-	this.broadcast_manager = new BroadcastManager(this);
-	this.air_manager = new AirManager(this);
+	this.init(user, admin, station);
+}
 
-	this.comment_manager = new CommentManager(this);
-	this.comment_manager.get(); // Force comments to be fetched
+StationClient.prototype = {
+	
+	init: function(user, admin, station){
+		this.user = user;
+		this.admin = admin;
+		this.station = station;
 
-	this.favorite_sdk = new FavoriteSDK(this.broadcast_manager);
+		this.broadcasts_counter = new Counter("#broadcasts");
+		this.views_counter = new Counter("#views");
+
+		this.broadcast_manager = new BroadcastManager(this);
+		this.air_manager = new AirManager(this);
+
+		this.comment_manager = new CommentManager(this);
+		this.comment_manager.get(); // Force comments to be fetched
+
+		this.favorite_sdk = new FavoriteSDK(this.broadcast_manager);
+	}
+	
 }
--- static/js/station/page/off.js
+++ static/js/station/page/off.js
@@ -1,8 +1,8 @@
-$(function(){
-	STATION_CLIENT = new StationClient(USER, ADMIN, STATION)
-})
+// ---------------------------------------------------------------------------
+// SPECIFIC OVERWRITING FUNCTIONS FOR IFRAME APP - "OFF" MODE
+// ---------------------------------------------------------------------------
 
-function StationClient(user, admin, station){
+StationClient.prototype.init = function(user, admin, station){
 	this.user = user;
 	this.admin = admin;
 	this.station = station;
