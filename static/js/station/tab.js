287cf1b253073581235aa492f1ea381cc6c1a11f
Damien Detcherry <damien@phonoblaster.com>
2012-02-29 10:27:14 +0100

    Compiled the tab displayer file and the tab manager file

--- static/js/station/tab.js
+++ static/js/station/tab.js
@@ -1,4 +1,50 @@
 // ---------------------------------------------------------------------------
+// TAB DISPLAYER
+// ---------------------------------------------------------------------------
+
+$(function(){
+	var tab = new TabDisplayer();
+});
+
+function TabDisplayer(){
+	this.init()
+}
+
+TabDisplayer.prototype = {
+	
+	init: function(){
+		//Find the tab that is current
+		current_tab = $("#tabs a.current").attr("href");
+		$(current_tab).show();
+		
+		// Listen for click on tabs.
+	 	$("#tabs a").bind("click",function() {
+			// If not current tab. 
+			if (!$(this).hasClass("current")) {
+				// Change the current indicator.
+				 $(this)
+					.addClass("current")
+					.parent("li")
+					.siblings("li")
+					.find("a.current")
+					.removeClass("current");
+				
+				// Show target, hide others.
+				 $($(this).attr('href'))
+					.show()
+					.siblings(".tab-content")
+					.hide();
+			}
+			
+			// Nofollow.
+			 this.blur();
+			 return false;
+		});
+		
+	},	
+}
+
+// ---------------------------------------------------------------------------
 // TAB MANAGER
 // ---------------------------------------------------------------------------
 
