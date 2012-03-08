c99b57c4a0cc31b0092740ad75e96fde0449e269
Damien Detcherry <damien@phonoblaster.com>
2012-03-08 10:01:57 +0100

    Bugfix: when GET on /api/air if station does not exist no bug

--- controllers/api/air.py
+++ controllers/api/air.py
@@ -9,16 +9,16 @@ class ApiAirHandler(BaseHandler):
 	def get(self):
 		shortname = self.request.get("shortname")		
 		station_proxy = StationApi(shortname)
-		queue = station_proxy.queue
+		if(station_proxy.station):
+			queue = station_proxy.queue
+			if(len(queue)>0):
+				data = {
+					"response": True,
+					"content": queue[0],
+				}
+			else:
+				data = {
+					"response": False,
+				}
 		
-		if(len(queue)>0):
-			data = {
-				"response": True,
-				"content": queue[0],
-			}
-		else:
-			data = {
-				"response": False,
-			}
-			
-		self.response.out.write(json.dumps(data))
+			self.response.out.write(json.dumps(data))
