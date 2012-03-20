9cdcec1c883b5443215c9882b66578746572ff35
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-03-20 17:34:27 +0100

    New Tape Model

--- unittests/models/tapeunittests.py
+++ unittests/models/tapeunittests.py
@@ -5,6 +5,8 @@
 import unittest
 from google.appengine.ext import testbed
 
+from models.db.track import Track
+from models.db.station import Station
 from models.db.tape import Tape
 from models.db.tape import CompilationTrack
 
@@ -23,4 +25,36 @@ class TapeUnitTest(unittest.TestCase):
 
 	def tearDown(self):
 		self.testbed.desactivate()
-		
+	
+	def testAddingTrackToCompilation(self):
+		#Creating Station
+		station = Station(
+			shortname = "testStation",
+			link = "https://www.facebook.com/pages/Fans-of-Old-School-Hip-Hop/129247123864094", 
+			name ="Test Station")
+		station.put()
+
+		#Creating 2 tracks
+		track_1 = Track(youtube_id ="iOKFiGyAmMQ", station = station )
+		track_1.put()
+
+		track_2 = Track(youtube_id ="Zdrga0MwoS8", station = station )
+		track_2.put()
+
+		#Creating a tape
+		tape = Tape(tape_name = "Test Tape")
+		tape.put()
+
+		#Adding tracks to tape
+		tape.tracks.append(track_1.key())
+		tape.tracks.append(track_2.key())
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
