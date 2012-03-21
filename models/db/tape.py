84561efcefda26cc782ff6d995347b3436cbc5a3
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-03-21 10:53:33 +0100

    Tape model updated, sation fild added

--- models/db/tape.py
+++ models/db/tape.py
@@ -7,12 +7,16 @@ from models.db.station import Station
 
 class Tape(db.Model):
 	"""
+		station - station associated to a tape
 		tape_name - name of the tape.
 		tape_thumbnail - thumbnail of the tape.
 		tracks - list of tracks in tape.
+		created - time of creation
+		updated - time of latest update
 
 	"""
 
+	station = db.ReferenceProperty(Station, required = True, collection_name = "tapesStation")
 	tape_name = db.StringProperty()
 	tape_thumbnail = db.BlobProperty(default = None) # TODO : BlobProperty / BlobStore, difference?
 	tracks = db.ListProperty(db.Key)
