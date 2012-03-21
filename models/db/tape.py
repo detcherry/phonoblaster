4c2f7c363e3518c0fa580f581e680c1e9e6cd128
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-03-21 10:49:10 +0100

    Tape model updated, created and updated filds added

--- models/db/tape.py
+++ models/db/tape.py
@@ -16,3 +16,5 @@ class Tape(db.Model):
 	tape_name = db.StringProperty()
 	tape_thumbnail = db.BlobProperty(default = None) # TODO : BlobProperty / BlobStore, difference?
 	tracks = db.ListProperty(db.Key)
+	created = db.DateTimeProperty(auto_now_add = True)
+	updated = db.DateTimeProperty(auto_now = True)
