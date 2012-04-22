80afbd9f87c2d3507395f0c678e136bdbdcb7c2d
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-04-22 22:59:03 +0200

    Starting to upgrade database access to Air

--- controllers/home.py
+++ controllers/home.py
@@ -13,21 +13,26 @@ from models.db.track import Youtube
 class HomeHandler(BaseHandler):
 	def get(self):
 		if(self.user_proxy):
-			
 			user = self.user_proxy.user
 			# User has been put less than 5 secs ago
 			if(user.created > datetime.utcnow() - timedelta(0,5)):
 				self.redirect("/station/create")
 			else:
 				user_stations = self.user_proxy.stations
-			
+
 				q = Air.all()
 				q.order("-created")
 				feed = q.fetch(50)
-			
+
 				youtube_ids = [Air.youtube_id.get_value_for_datastore(f) for f in feed]
-				extended_tracks = Youtube.get_extended_tracks(youtube_ids) #TO BE REMOVED
-			
+				extended_tracks = []
+				for air in feed:
+					extended_tracks.append({
+						"id": air.youtube_id,
+						"title": air.youtube_title,
+						"duration": air.youtube_duration,
+					})
+
 				on_air = []
 				stations = []
 				tracks = []
