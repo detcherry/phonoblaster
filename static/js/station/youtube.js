1c4ea4abd2f26df240310961c53d7fed99c4802e
Damien Detcherry <damien@phonoblaster.com>
2012-05-28 12:26:58 +0200

    Youtube videos quality forced to medium

--- static/js/station/youtube.js
+++ static/js/station/youtube.js
@@ -26,6 +26,13 @@ YoutubeManager.prototype = {
 		// Player already loaded
 		try{
 			ytplayer.loadVideoById(youtube_id, start);
+			setTimeout(function(){
+				// If medium quality available
+				if(ytplayer.getAvailableQualityLevels().indexOf("medium") != -1){
+					ytplayer.setPlaybackQuality("medium");
+				}
+			},1000)
+			
 			PHB.log("Player already loaded");
 		}
 		// Player not loaded yet
@@ -120,6 +127,12 @@ function onYouTubePlayerReady(playerId) {
 	
 	// Triggers the video
 	ytplayer.loadVideoById(YOUTUBE_ID, START);
+	setTimeout(function(){
+		// If medium quality available
+		if(ytplayer.getAvailableQualityLevels().indexOf("medium") != -1){
+			ytplayer.setPlaybackQuality("medium");
+		}
+	},1000)
 
 	//If the volume had been turned off, mute the player
 	if(!VOLUME){
