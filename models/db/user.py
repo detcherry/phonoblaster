80bc2becdc5a5ad473c55afc1df248012872b05d
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-06-06 22:30:05 +0200

    Changing User datastore model

--- models/db/user.py
+++ models/db/user.py
@@ -5,6 +5,7 @@ class User(db.Model):
     first_name = db.StringProperty(required = True)
     last_name = db.StringProperty(required = True)
     email = db.EmailProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
     stations = db.ListProperty(db.Key)
+    profile = db.ReferenceProperty(Station, required = False, collection_name = "userProfile")
+    created = db.DateTimeProperty(auto_now_add = True)
     updated = db.DateTimeProperty(auto_now = True)
