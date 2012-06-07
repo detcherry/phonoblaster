ec395073ff2503b55d6cd1c69cd60a6b0c8b9aa1
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-06-07 18:32:16 +0200

    Session datastore model changed

--- models/db/session.py
+++ models/db/session.py
@@ -8,47 +8,46 @@ from models.db.station import Station
 class Session(db.Model):
 	# key_name = channel_id
 	channel_token = db.StringProperty(required = True)
-	user = db.ReferenceProperty(User, required = False, collection_name = "sessionUser")
-	station = db.ReferenceProperty(Station, required = True, collection_name = "sessionStation")
+	listener = db.ReferenceProperty(Station, required = False, collection_name = "sessionListener")
+	host = db.ReferenceProperty(Station, required = True, collection_name = "sessionHost")
 	created = db.DateTimeProperty(auto_now_add = True)
 	ended = db.DateTimeProperty()
 	
-	# TO BE CHANGED
 	@staticmethod
 	def get_extended_sessions(sessions):		
-		user_keys = []
-		users = []
-		user_sessions = []
+		listeners_keys = []
+		listeners = []
+		listener_sessions = []
 		
-		anonymous_users = []
+		anonymous_listeners = []
 		anonymous_sessions = []
 		
 		for s in sessions:
-			user_key = Session.user.get_value_for_datastore(s)
-			if user_key is not None:
-				user_keys.append(user_key)
-				user_sessions.append(s)
+			listener_key = Session.listener.get_value_for_datastore(s)
+			if listener_key is not None:
+				listeners_keys.append(listener_key)
+				listener_sessions.append(s)
 			else:
-				anonymous_users.append(None)
+				anonymous_listeners.append(None)
 				anonymous_sessions.append(s)
 		
-		users = db.get(user_keys)
+		listeners = db.get(listeners_keys)
 		
-		user_extended_sessions = [Session.get_extended_session(s, u) for s, u in zip(user_sessions, users)]
-		anonymous_extended_sessions = [Session.get_extended_session(s, au) for s, au in zip(anonymous_sessions, anonymous_users)]
-		extended_sessions = user_extended_sessions + anonymous_extended_sessions
+		listener_extended_sessions = [Session.get_extended_session(s, l) for s, l in zip(user_sessions, listeners)]
+		anonymous_extended_sessions = [Session.get_extended_session(s, al) for s, al in zip(anonymous_sessions, anonymous_listeners)]
+		extended_sessions = listener_extended_sessions + anonymous_extended_sessions
 		
 		return extended_sessions
-	# TO BE CHANGED
+	
 	@staticmethod
-	def get_extended_session(session, user):
-		if(user):	
+	def get_extended_session(session, listener):
+		if(listener):	
 			extended_session = {
 				"key_name": session.key().name(),
 				"created": timegm(session.created.utctimetuple()),
-				"listener_key_name": user.key().name(),
-				"listener_name": user.first_name + " " + user.last_name,
-				"listener_url": "/user/" + user.key().name(),
+				"listener_key_name": listener.key().name(),
+				"listener_name": listener.first_name + " " + listener.last_name,
+				"listener_url": "/" + listener.key().name(),
 			}
 		else:
 			extended_session = {
