34b0eac8061ef08cb4193b7a866e5d24d8100cc1
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-06-07 13:49:21 +0200

    New datastore model for station

--- models/db/station.py
+++ models/db/station.py
@@ -5,8 +5,9 @@ class Station(db.Model):
 	# key_name = facebook_id (page)
 	shortname = db.StringProperty(required = True)
 	name = db.StringProperty(required = True)
-	link = db.StringProperty(required = True) # Link of the facebook page
+	link = db.StringProperty(required = False) # Link of the facebook page
 	created = db.DateTimeProperty(auto_now_add = True)
 	updated = db.DateTimeProperty(auto_now = True)
 	broadcasts = db.ListProperty(db.Key)
 	timestamp = db.DateTimeProperty(auto_now_add=True)
+	type = db.StringProperty(required=False, choices=set(["user", "page"]))
