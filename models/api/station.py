b015931a89e833dcb17f16894b94dd4aa98c749a
Damien Detcherry <damien@phonoblaster.com>
2012-06-15 18:44:28 +0200

    Removed comment

--- models/api/station.py
+++ models/api/station.py
@@ -70,7 +70,6 @@ class StationApi():
 				logging.info("Station already in memcache")	
 		return self._station
 	
-	#def put_station(self, id, shortname, name, link, type):
 	def put_station(self, id, shortname, name, link, type, full, thumb):
 		station = Station(
 			key_name = id,
