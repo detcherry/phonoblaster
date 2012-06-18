ac3aa1f5f81853fc07890511fa80f7ce643e1013
Damien Detcherry <damien@phonoblaster.com>
2012-06-18 11:30:39 +0200

    Changed destination url for background edit

--- controllers/station/station.py
+++ controllers/station/station.py
@@ -1,17 +1,22 @@
 import logging
 
 from controllers.base import profile_required
-
 from controllers.station.root import RootHandler
-
 from models.api.station import StationApi
+
+from google.appengine.ext import blobstore
 
 class StationHandler(RootHandler):
 	@profile_required
 	def get(self, shortname):
 		self.station_proxy = StationApi(shortname)
-		
+	
 		if(self.station_proxy.station):
-			self.render("station/station.html", None)
+			url = "/api/" + shortname + "/edit"
+			template_values = {
+				"blobstore_url": blobstore.create_upload_url(url),
+			}
+
+			self.render("station/station.html", template_values)
 		else:
 			self.render("station/404.html", None)
