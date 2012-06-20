226ba152339e09cbc27991ae6519e8a7826124b9
Damien Detcherry <damien@phonoblaster.com>
2012-06-20 17:51:28 +0200

    Bugfix: redirect was getting unicode and raised error (probably due to python2.7 migration)

--- controllers/station/picture.py
+++ controllers/station/picture.py
@@ -5,6 +5,6 @@ class StationPictureHandler(RootHandler):
 	def get(self, shortname):
 		self.station_proxy = StationApi(shortname)
 		facebook_id = self.station_proxy.station.key().name()
-		facebook_url = "https://graph.facebook.com/"+ facebook_id +"/picture?type=large"
-		self.redirect(facebook_url)
+		facebook_url = "https://graph.facebook.com/"+ facebook_id +"/picture?type=large"		
+		self.redirect(str(facebook_url))
 	
