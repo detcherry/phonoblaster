39d99da2e5d908305a5ce323d929300acdd1e3a9
Damien Detcherry <damien@phonoblaster.com>
2012-06-20 16:48:14 +0200

    Access to db upgrade restricted to admins

--- controllers/admin/upgrade.py
+++ controllers/admin/upgrade.py
@@ -1,14 +1,16 @@
 import logging
 
 from controllers.base import BaseHandler
-from controllers.base import login_required
+from controllers.base import admin_required
 
 from google.appengine.api.taskqueue import Task
 
 class AdminUpgradeHandler(BaseHandler):
+	@admin_required
 	def get(self):
 		self.render("admin/dbupgrade.html", None)
 	
+	@admin_required
 	def post(self):
 		task = Task(
 			url = "/taskqueue/upgrade",
