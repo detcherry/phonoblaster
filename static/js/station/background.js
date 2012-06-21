dd38756fc898c8c40aca634c9c15c0ce837f5520
Damien Detcherry <damien@phonoblaster.com>
2012-06-21 10:06:01 +0200

    Background img resizing done well cross browsers (used an external plugin)

--- static/js/station/background.js
+++ static/js/station/background.js
@@ -1,4 +1,55 @@
 // ---------------------------------------------------------------------------
+// BACKGROUND RESIZING
+// ---------------------------------------------------------------------------
+
+/**
+ * jQuery.fullBg
+ * Version 1.0
+ * Copyright (c) 2010 c.bavota - http://bavotasan.com
+ * Dual licensed under MIT and GPL.
+ * Date: 02/23/2010
+**/
+
+(function($){
+	$.fn.fullBg = function(){
+		var bgImg = $(this);
+		
+		function resizeImg(){
+			var imgwidth = bgImg.width();
+			var imgheight = bgImg.height();
+			
+			var winwidth = $(window).width();
+			var winheight = $(window).height();
+			
+			var widthratio = winwidth / imgwidth;
+			var heightratio = winheight / imgheight;
+			
+			var widthdiff = heightratio * imgwidth;
+			var heightdiff = widthratio * imgheight;
+			
+			if(heightdiff>winheight){
+				bgImg.css({
+					width: winwidth+'px',
+					height: heightdiff+'px'
+				});
+		    }
+			else{
+				bgImg.css({
+					width: widthdiff+'px',
+					height: winheight+'px'
+				});
+			}
+		} 
+	
+    	resizeImg();
+
+    	$(window).resize(function(){
+			resizeImg();
+		});
+	};
+})(jQuery)
+
+// ---------------------------------------------------------------------------
 // BACKGROUND MANAGER
 // ---------------------------------------------------------------------------
 
@@ -18,6 +69,10 @@ BackgroundManager.prototype.init = function(client){
 	
 	// Init methods
 	this.uploadListen();
+	
+	// Init background size
+	$("#background img").fullBg();
+	$("#background img").show();
 }
 
 BackgroundManager.prototype.uploadListen = function(){
