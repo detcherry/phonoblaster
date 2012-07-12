f1e22e5ef203f403c28efa023b43fcb8a816d5ea
Damien Detcherry <damien@phonoblaster.com>
2012-07-12 11:19:21 +0200

    Recommendations works with the new API system

--- static/js/station/recommendations.js
+++ static/js/station/recommendations.js
@@ -21,9 +21,7 @@ function RecommendationManager(client){
 	this.closeListen();
 }
 
-RecommendationManager.prototype.dispatch = function(){
-	$("#popup-recommendations h3 strong").html("Facebook")
-	
+RecommendationManager.prototype.dispatch = function(){	
 	var that = this;
 	// Call Facebook API to know which tracks the user has posted on his wall
 	FACEBOOK.retrieveWallLinks(function(items){
@@ -33,47 +31,85 @@ RecommendationManager.prototype.dispatch = function(){
 
 RecommendationManager.prototype.filterFacebook = function(items){
 	var youtube_ids = []
+	var soundcloud_urls = []
 	
-	$.each(items, function(i, item){
-		var re = RegExp("http://www.youtube.com/watch\\?v=([\\w_]+)","g")
+	$.each(items, function(i, item){		
+		var re = RegExp("http://www.youtube.com/watch\\?v=([\\w_-]+)","g")
 		var m = re.exec(item.link)
 		if(m!= null){
 			youtube_ids.push(m[1])
 		}
+		
+		var re2 = RegExp("http://soundcloud.com/(.*)","g")
+		var m2 = re2.test(item.link)
+		if(m2){
+			soundcloud_urls.push(item.link)
+		}
 	})
-	
-	if(youtube_ids.length > 0){
-		this.filterYoutube(youtube_ids)
-	}
-}
-
-RecommendationManager.prototype.filterYoutube = function(youtube_ids){
-	var data = {
-		"youtube_ids": youtube_ids.join(","),
-	}
+		
 	var that = this;
-	
-	$.ajax({
-		url: "/api/recommendations",
-		dataType: "json",
-		timeout: 60000,
-		data: data,
-		error: function(xhr, status, error){
-			PHB.log('An error occurred: ' + error + '\nPlease retry.');
-		},
-		success: function(json){
-			if(json.length > 0){
-				// Remove volume
-				$("#media-volume").trigger("click");
-								
-				that.empty(function(){
-					that.getCallback(json);
-				})
+	// Get info from Soundcloud
+	this.filterSoundcloud(soundcloud_urls, function(soundcloud_tracks){
 				
+		// Get info from Youtube
+		that.filterYoutube(youtube_ids, function(youtube_tracks){
+			
+			var tracks = soundcloud_tracks.concat(youtube_tracks);
+			if(tracks.length > 0){
+				
+				// Remove volume
+				$("#media-volume a").trigger("click");
+
+				that.empty(function(){
+					that.getCallback(tracks);
+				})
+
 				that.displayPopup();
 			}
-		}
-	})	
+		})
+	})
+}
+
+RecommendationManager.prototype.filterSoundcloud = function(soundcloud_urls, callback){
+	
+	var soundcloud_tracks = [];
+	if(soundcloud_urls.length >0){
+		// TODO: Resolve Soundcloud urls when it's possible to do it in one GET with their API
+	
+		callback(soundcloud_tracks)
+	}
+	else{
+		callback(soundcloud_tracks)
+	}
+	
+}
+
+//RecommendationManager.prototype.filterYoutube = function(youtube_ids){
+RecommendationManager.prototype.filterYoutube = function(youtube_ids, callback){
+	
+	var youtube_tracks = []
+	
+	if(youtube_ids.length > 0){
+		$.ajax({
+			url: "/api/recommendations",
+			dataType: "json",
+			timeout: 60000,
+			data: {
+				"youtube_ids": youtube_ids.join(","),
+			},
+			error: function(xhr, status, error){
+				PHB.log('An error occurred: ' + error + '\nPlease retry.');
+			},
+			success: function(json){
+				youtube_tracks = json;
+				callback(youtube_tracks);
+			}
+		})
+	}
+	else{
+		callback(youtube_tracks)
+	}
+
 }
 
 RecommendationManager.prototype.displayPopup = function(){
@@ -90,7 +126,7 @@ RecommendationManager.prototype.closeListen = function(){
 		$.fancybox.close(true);
 		
 		// Put volume back
-		$("#media-volume").trigger("click");
+		$("#media-volume a").trigger("click");
 	})
 }
 
@@ -98,10 +134,11 @@ RecommendationManager.prototype.serverToLocalItem = function(content){
 
 	// Tracks fetched from Facebook + Youtube
 	new_track = {
-		"type": "track",
-		"youtube_id": content.id, 
-		"youtube_title": content.title, 
-		"youtube_duration": content.duration,
+		"type": "youtube",
+		"id": content.id, 
+		"title": content.title, 
+		"duration": content.duration,
+		"thumbnail": "https://i.ytimg.com/vi/" + content.id + "/default.jpg",
 		"track_id": null,
 		"track_created": null,
 		"track_submitter_key_name": this.client.host.key_name,
@@ -110,7 +147,7 @@ RecommendationManager.prototype.serverToLocalItem = function(content){
 	};
 	
 	var item = {
-		id: new_track.youtube_id,
+		id: new_track.id,
 		created: new_track.created,
 		content: new_track,
 	}
@@ -122,32 +159,31 @@ RecommendationManager.prototype.UIBuild = function(item){
 	var id = item.id;
 	var content = item.content;
 	
-	var youtube_id = content.youtube_id;
-	var youtube_title = content.youtube_title;
-	var youtube_duration = PHB.convertDuration(content.youtube_duration)
-	var youtube_thumbnail = "https://i.ytimg.com/vi/" + youtube_id + "/default.jpg";
-	
-	var process_action = "Add"
-	
+	var type = content.type;
+	var id = content.id;
+	var title = content.title;
+	var duration = PHB.convertDuration(content.duration)
+	var thumbnail = content.thumbnail;
+		
 	var div = $("<div/>").addClass("item").attr("id",id)
 	div.append(
 		$("<div/>")
 			.addClass("item-picture")
-			.append($("<img/>").attr("src", youtube_thumbnail))
+			.append($("<img/>").attr("src", thumbnail).addClass(type))
 	)
 	.append(
 		$("<div/>")
 			.addClass("item-title")
-			.append($("<span/>").addClass("middle").html(youtube_title))
+			.append($("<span/>").addClass("middle").html(title))
 	)
 	.append(
 		$("<div/>")
 			.addClass("item-subtitle")
-			.append($("<div/>").addClass("item-duration").html(youtube_duration))
+			.append($("<div/>").addClass("item-duration").html(duration))
 			.append(
 				$("<div/>")
 					.addClass("item-process")
-					.append($("<a/>").addClass("btn").attr("name",id).html(process_action))
+					.append($("<a/>").addClass("btn").attr("name",id).html("Add"))
 			)
 	)
 				
